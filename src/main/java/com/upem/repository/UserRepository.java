fa6fc694f51285b2e305d5db80fe43bb168da8eb
yassinecheggar <cheggar13@gmail.com>
2020-03-07 01:03:20 +0100

    fix

--- src/main/java/com/upem/repository/UserRepository.java
+++ src/main/java/com/upem/repository/UserRepository.java
@@ -10,10 +10,10 @@ import com.upem.models.User;
 public interface UserRepository extends CrudRepository<User, Integer>{
 	
 	
-	@Query(value = "SELECT * FROM iot.user where mail=?1 and mdp =?2", nativeQuery = true)
+	@Query(value = "SELECT * FROM user where mail=?1 and mdp =?2", nativeQuery = true)
 	User login(String mail , String mdp);
 	
-	@Query(value = "SELECT * FROM iot.user where id =?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM user where id =?1", nativeQuery = true)
 	User getbyId(Integer id);
 
 }
