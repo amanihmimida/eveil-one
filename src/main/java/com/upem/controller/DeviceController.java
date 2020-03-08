df2d8e648584b20bc879bd8aec3ead27f3c3a14e
yassinecheggar <cheggar13@gmail.com>
2020-03-08 01:50:19 +0100

    add delete device

--- src/main/java/com/upem/controller/DeviceController.java
+++ src/main/java/com/upem/controller/DeviceController.java
@@ -1,5 +1,6 @@
 package com.upem.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,40 +20,66 @@ import com.upem.repository.UserRepository;
 @RestController
 @RequestMapping("/api/Device")
 public class DeviceController {
-	
+
 	@Autowired
 	UserRepository repo;
 	@Autowired
 	DeviceRepository repoDev;
-	
+
 	@PutMapping("/AddUserDevice/{id}")
 	public String AddUserDevice(@PathVariable Integer id, @RequestBody Device dev)
 	{
-		 User A= repo.getbyId(id);
-		 if(A!=null) {
+		User A= repo.getbyId(id);
+		if(A!=null) {
 			Device D =  repoDev.getbyId(dev.getIdf(), dev.getCle());
-			 if(D!=null) {
-				 List<Device> lisDevices = A.getUserDevices();
-				 
-				 lisDevices.add(D);
-				 repo.save(A);
-				 return "ok";
-			 } 
-		 }
-		 return "not";
+			if(D!=null) {
+				List<Device> lisDevices = A.getUserDevices();
+
+				lisDevices.add(D);
+				repo.save(A);
+				return "ok";
+			} 
+		}
+		return "not";
 	}
-	
+
 	@PostMapping("/add")
 	public String addDevice(@RequestBody Device dev) {
-		
+
 		repoDev.save(dev);
 		return "ok";
 	}
-	
+
 	@GetMapping("/getDeviceUser/{id}")
 	public List<Device> getDeviceUser(@PathVariable Integer id) {
-		
+
 		return repo.getbyId(id).getUserDevices();
 	}
-	
+
+	@PostMapping("/deleteDeviceUser/{id}")
+	public String delete(@PathVariable Integer id,@RequestBody Device device  ) {
+
+		User A= repo.getbyId(id);
+		List<Device> devices  =   A.getUserDevices();
+		boolean stat =false;
+
+		for (Iterator<Device> iter = devices.listIterator(); iter.hasNext(); ) {
+			Device D = iter.next();
+			if(device.getId() == D.getId()) {
+
+				iter.remove();
+				stat = true;
+				repo.save(A);
+
+			}
+		}
+
+
+
+
+		if(stat == true)return "ok";
+
+		return "not";
+	}
+
 }
